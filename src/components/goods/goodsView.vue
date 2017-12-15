64c64be9a783ff971febbe422cda1fb54a46c022
gaosheng <245607470@qq.com>
2017-12-15 22:00:10 +0800

    2017-12-15  my PC commit

--- src/components/goods/goodsView.vue
+++ src/components/goods/goodsView.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" ref="menuWrapper">
       <ul>
-        <li v-for="item in goods" class="menu-item">
+        <li v-for="(item, index) in goods" class="menu-item" :class="{'current': currentIndex === index}" @click="selectMenu(index, $event)">
           <span class="text border-1px">
             <span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span>{{ item.name }}
           </span>
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" ref="foodsWrapper">
       <ul>
-        <li v-for="item in goods" class="food-list">
+        <li v-for="item in goods" class="food-list food-list-hook">
           <h1 class="title">{{ item.name }}</h1>
           <ul>
             <li v-for="(food, index) in item.foods" class="food-item border-1px" :class="{'last-child':index == item.foods.length - 1}">
@@ -22,12 +22,10 @@
                 <h2 class="name">{{ food.name }}</h2>
                 <p class="desc">{{ food.description }}</p>
                 <div class="extra">
-                  <span>月售{{ food.sellCount }}份</span>
-                  <span>好评率{{ food.rating }}%</span>
+                  <span class="count">月售{{ food.sellCount }}份</span><span>好评率{{ food.rating }}%</span>
                 </div>
                 <div class="price">
-                  <span>￥{{ food.price }}</span>
-                  <span v-show="food.oldPrice">
+                  <span class="now">￥{{ food.price }}</span><span class="old" v-show="food.oldPrice">
                     ￥{{ food.oldPrice }}
                   </span>
                 </div>
@@ -41,20 +39,72 @@
 </template>
 
 <script>
+import BScroll from 'better-scroll'
+
 export default {
   name: 'goodsView',
   data() {
     return {
       classMap: [],
-      goods: {}
+      goods: {},
+      listHeight: [],
+      scrollY: 0
+    }
+  },
+  computed: {
+    currentIndex() {
+      for (let i = 0; i < this.listHeight.length; i++) {
+        let height = this.listHeight[i];
+        let fheight = this.listHeight[i + 1];
+        if (!fheight || (this.scrollY >= height && this.scrollY < fheight)) {
+          return i
+        }
+      }
+      return 0;
     }
   },
   created() {
     this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'],
     this.$http.get('http://localhost:3000/goods').then((response) => {
       var response = response.body;
-      this.goods = response
+      this.goods = response;
+      this.$nextTick(() => {
+        this._initScroll();
+        this._calculateHeight();
+      })
     })
+  },
+  methods: {
+    selectMenu(index, e) {
+      if(!e._constructed) {
+        return;
+      }
+      let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+      let el = foodList[index];
+      this.foodsScroll.scrollToElement(el, 500);
+    },
+    _initScroll() {
+      this.menuScroll = new BScroll(this.$refs.menuWrapper, {
+        click: true
+      });
+      this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+        probeType: 3
+      });
+
+      this.foodsScroll.on('scroll', (pos) => {
+        this.scrollY = Math.abs(Math.round(pos.y));
+      })
+    },
+    _calculateHeight() {
+      let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+      let height = 0;
+      this.listHeight.push(height);
+      for (let i = 0; i < foodList.length; i++) {
+        let item = foodList[i];
+        height += item.clientHeight;
+        this.listHeight.push(height);
+      }
+    }
   }
 }
 </script>
@@ -73,13 +123,20 @@ export default {
       flex 0 0 80px
       width 80px
       background #f3f5f7
-      overflow auto
       .menu-item
         display table
         height 54px
         width 56px
         line-height 14px
-        margin auto
+        padding 0 12px
+        &.current
+          position relative
+          margin-top -1px
+          z-index 10
+          background #fff
+          font-weight 700
+          .text
+            border-none()
         .text
           display table-cell
           width 56px
@@ -105,7 +162,6 @@ export default {
               bg-image('guarantee_3')
     .foods-wrapper
       flex 1
-      overflow-x hidden
       .title
         padding-left 14px
         height 26px
@@ -139,8 +195,20 @@ export default {
             color rgba(147, 153, 159, 1)
           .desc
             margin-bottom 8px
+            line-height 12px
           .extra
-            line-height 10px
-            &.line-count
+            line-height 12px
+            .count
               margin-right 12px
+          .price
+            font-weight 700
+            line-height 24px
+            .now
+              margin-right 8px
+              font-size 14px 
+              color rgba(240, 20, 20, 1)
+            .old
+              text-decoration line-through
+              font-size 10px
+              color rgba(147, 153, 159, 1)
 </style>
